b7ccdd4e17dd54d6625bd5e92feafdea1f3101fe
Lashonda Miller <lashondamiller007@gmail.com>
2020-07-11 22:48:17 -0400

    arrow functions

---
+++ js/app.js
@@ -0,0 +1,54 @@
+//regular functions
+
+var bruh = 78;
+var broke = 79;
+
+function add(boy, girl) {
+    return boy + girl ;
+}
+
+console.log(add(bruh,broke));
+
+//
+
+function sub(boy3, girl4) {
+    return boy3 - girl4 ;
+}
+
+console.log(sub(bruh,broke))
+
+//
+
+function mult(boy5, girl6) {
+    return boy5 * girl6 ;
+}
+
+console.log(mult(bruh,broke))
+
+//
+
+function divi(boy7, girl8) {
+    return boy7 / girl8 ;
+}
+
+console.log(divi(bruh,broke))
+
+// Arrow Functions
+
+var apple = (Y, Z) => {return Y + Z}
+    console.log(apple(99,1))
+
+//
+
+var pear = (W, X) => {return W - X}
+console.log(pear(65,29))
+
+//
+
+var chicken = (R, S) => {return S * R}
+console.log(chicken(100,100))
+
+//
+
+var popcorn = (T, V) => {return T / V}
+console.log(popcorn(5,2))
